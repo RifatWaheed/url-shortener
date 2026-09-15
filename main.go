a8cb993f7ab7ce7020e6cd1832266eb838c80c53
Rifat Waheed <waheed@celimited.com>
2026-09-15 17:15:05 +0600

    pgx connection established

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"time"
+
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Printf("unable to find env variables : %v", err)
+	}
+	pool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("unable to create connection pool: %v", err)
+	}
+	defer pool.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := pool.Ping(ctx); err != nil {
+		log.Fatalf("unable to reach database : %v", err)
+	}
+
+	log.Println("connected to database")
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/health", healthHandler)
